40014612d4bd2bdda364b843e2521526cf05d84a
KoHtet <kokoh6329@gmail.com>
2022-09-15 21:48:55 +0630

    change about img

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,5 @@ export { default as line } from "./line.png";
 export { default as portfolio } from "./portfolio.png";
 export { default as starwar } from "./starwar.png";
 export { default as space } from "./space.png";
-export { default as kkh } from "./kkh.svg";
+export { default as kkh } from "./profile.jpg";
 export { default as ecommerce } from "./ecommerce.png";
